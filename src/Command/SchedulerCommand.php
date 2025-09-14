c7721e26fc0e44093d8e03965d724c27ff6d1d60
francois vaillant <frvaillant@gmail.com>
2025-09-15 00:03:47 +0200

    Feature: remove locker

--- src/Command/SchedulerCommand.php
+++ src/Command/SchedulerCommand.php
@@ -17,8 +17,6 @@ use Symfony\Component\Scheduler\Attribute\AsPeriodicTask;
 #[AsPeriodicTask(frequency: 20)]
 class SchedulerCommand extends Command
 {
-
-    use LockableTrait;
     const string FILENAME = 'index2.txt';
 
     public function __construct(private readonly SchedulerTestCommandService $commandService)
@@ -29,13 +27,7 @@ class SchedulerCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
 
-        if (!$this->lock()) {
-            return Command::SUCCESS;
-        }
-
         $this->commandService->execute(self::FILENAME);
-
-        $this->release();
 
         return Command::SUCCESS;
     }
